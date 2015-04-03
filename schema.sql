0d3df17c0a692c81e8f9634ff935d5d672e12d24
Justin <justinwhite321@gmail.com>
2015-04-03 15:27:44 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,34 +1,33 @@
+CREATE SCHEMA dumptruck;
+
 -- track all objects across all time series using this id
-CREATE TABLE data_master (
+CREATE TABLE dumptruck.data_master (
   id SERIAL PRIMARY KEY
   , external_id_0 TEXT
   , external_id_1 TEXT
-  , .. 
 );
 
--- basic contact information for each data source provider
-CREATE TABLE providers (
+-- basic contact information for each data source 
+CREATE TABLE dumptruck.sources (
   name TEXT PRIMARY KEY
+  , contact_name TEXT
   , contact_email TEXT
+  , contact_phone TEXT
   , description TEXT
-  , ..
 );
 
 -- basic information for each data series
-CREATE TABLE data_sets (
-  data_series_name TEXT PRIMARY KEY
-  , provider TEXT REFERENCES providers (name)
-  , source TEXT -- 2nd level description of where data came from
+CREATE TABLE dumptruck.data_sets (
+  name TEXT PRIMARY KEY
+  , source TEXT REFERENCES sources (name)
   , license TEXT
-  , ingest_script TEXT
   , duplicate_of TEXT REFERENCES data_sets (data_series_name)
-  , update_interval INTERVAL
-  , ..
+  , last_updated TIMESTAMP
 );
 
--- create by some ingest script
-CREATE TABLE data_series_1 (
+-- each series should look like this
+CREATE TABLE dumptruck.series_1 (
   id INT REFERENCES data_master (id)
   , ts TIMESTAMP WITHOUT TIME ZONE -- store all in UTC
-  , val ANY -- actual val will be typed
+  , val NUMERIC -- should be appropriate type for series
 );
