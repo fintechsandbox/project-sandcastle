bb89ae3549eeea26837fe4a31db72053040167e0
Sergey Privalov <privalov.sergey@gmail.com>
2016-04-15 21:28:38 +0300

    require configuration.rb

--- xignite-rb/lib/xignite.rb
+++ xignite-rb/lib/xignite.rb
@@ -4,6 +4,7 @@ require 'xignite/currency_service'
 require 'xignite/fund_asset_service'
 require 'xignite/history_data_service'
 require 'xignite/master_data_service'
+require 'xignite/configuration'
 
 module Xignite
   # Your code goes here...
